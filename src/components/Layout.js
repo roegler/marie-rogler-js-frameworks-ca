5588f0a0ad38339bd77b188bd2b81c95b3b3aa2a
Marie Røgler <mroegler@gmail.com>
2020-10-12 10:17:58 +0200

    connecting to api, adding code to home.js and layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink,
+} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+
+import Home from "../pages/Home"
+import Contact from "../pages/Contact"
+import Details from "../pages/Details"
+
+function Layout() {
+    return (
+        <Router>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <NavLink to="/" exact>
+                    <Navbar.Brand>CA</Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavLink to="/" exact className="nav-link">
+                            Home
+                        </NavLink>
+                        <NavLink to="/news" className="nav-link">
+                            Contact
+                        </NavLink>
+                        <NavLink to="/login" className="nav-link">
+                            Details
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Container>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/news" component={Contact} />
+                    <Route path="/login" component={Details} />
+                </Switch>
+            </Container>
+        </Router>
+    );
+}
+
+export default Layout;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,40 @@
 import React from 'react'
+import { BASE_URL } from "../components/Api";
+
+function GameList() {
+    // Storage
+    const [games, setGames] = useState([]);
+    const [filtredGames, setFilteredGames] = useState([]);
+
+    // Getting the games from API
+    useEffect(() => {
+        fetch(BASE_URL)
+            .then(response => response.json())
+            .then(json => {
+                setGames(json.results)
+                setFilteredGames(json.results)
+            })
+            .catch(error => console.log(error));
+    }, [])
+
+    const searchGames = function (event) {
+        console.log(event.target.value)
+        const searchText = event.target.value.toLowerCase();
+        const filteredArray = games.filter(g => {
+            return g.title.toLowerCase().startsWith(searchText)
+        })
+        setFilteredGames(filteredArray);
+    }
+
+    // Using the stuff in storage
+    return (
+        <div>
+            <SearchGame handleSearch={searchGames} />
+            {filtredGames.map(g => (
+                <GameItem key={g.name} background_image={g.background_image} rating={g.rating} released={g.released}/>
+            ))}
+        </div>
+    );
+}
+
+export default GameList;
